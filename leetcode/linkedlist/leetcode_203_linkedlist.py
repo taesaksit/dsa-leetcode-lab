b16d0999cc587627b33d5fafb7f59b80fe5f6e3a
tae@mac <taesaksit@gmail.com>
2025-09-02 19:09:28 +0700

    Remove Linked List Elements

--- leetcode/linkedlist/leetcode_203_linkedlist.py
+++ leetcode/linkedlist/leetcode_203_linkedlist.py
@@ -1,29 +1,45 @@
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
-        
+
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        return
-    
-    def createHead(self, arr) :
+
+        dummy = ListNode()
+        dummy.next = head
+        current = dummy
+
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummy.next
+
+    def createHead(self, arr):
         head = ListNode(arr[0])
         curr = head
-        for i in  arr[1:] :
+        for i in arr[1:]:
             curr.next = ListNode(i)
             curr = curr.next
-        return  head
-    
-    
-input = [1,2,6,3,4,5,6]
-sol = Solution()
-head = sol.createHead(input)
+        return head
 
-curr = head
-while curr :
+
+# nums = [7, 7, 7, 7]
+nums = [1,2,6,3,4,5,6]
+sol = Solution()
+head = sol.createHead(nums)
+result = sol.removeElements(head, 6)
+
+curr = result
+
+while curr:
     print(curr.val)
     curr = curr.next
