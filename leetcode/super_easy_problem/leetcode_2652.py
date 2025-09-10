8779a2db1b95b4ae235ab08131a13e2fcdf6b1f6
tae@pc <taesaksit09@gmail.com>
2025-09-10 11:38:35 +0700

    Sum Multiples

---
+++ leetcode/super_easy_problem/leetcode_2652.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        total = 0
+        for i in range(1, n+1):
+            if i%3== 0 or i%5==0 or i%7==0:
+                total += i
+        return total
+
+
+sol = Solution()
+result = sol.sumOfMultiples(7)
+print(result)
